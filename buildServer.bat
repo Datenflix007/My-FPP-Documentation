ed1a64d4df863730b7b71620e89837f14ae5817d
FeSta <92328872+Datenflix007@users.noreply.github.com>
2024-10-20 17:03:09 +0200

    Update buildServer.bat
    the changes implements the generating of a .jar file for Server

--- buildServer.bat
+++ buildServer.bat
@@ -1,4 +1,27 @@
-javac -d classes/ src/*.java
+@echo off
+REM Kompiliere Java-Dateien
+echo Kompiliere Java-Dateien...
+REM mkdir classes
+javac -d classes src/*.java
+
 cd classes/
-java Server Room User Message FileHandler Log DirectChat DirectMessage
+REM Erstelle die Manifestdatei
+REM echo Main-Class: Server > ServerManifest.txt
+
+REM Erstelle die JAR-Datei im Verzeichnis der .class-Dateien und füge die Manifestdatei hinzu
+
+jar cfm Server.jar ServerManifest.txt *.class
+
+REM Überprüfe, ob die JAR-Datei erfolgreich erstellt wurde
+if exist Server.jar (
+    echo JAR-Datei Server.jar wurde erfolgreich erstellt.
+) else (
+    echo Fehler beim Erstellen der JAR-Datei!
+)
+
+REM Starte den Server
+echo Starte Server...
+java -jar Server.jar
+
 cd ../
+pause
