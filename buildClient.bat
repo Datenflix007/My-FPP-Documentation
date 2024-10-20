cf27200afb2e2e0907c0c27b20196d2d3f1a42fe
FeSta <92328872+Datenflix007@users.noreply.github.com>
2024-10-20 17:03:44 +0200

    Update buildClient.bat
    generates a .jar file

--- buildClient.bat
+++ buildClient.bat
@@ -1,4 +1,27 @@
-javac -Xlint:unchecked -d classes/ src/*.java
+@echo off
+REM Kompiliere Java-Dateien
+echo Kompiliere Java-Dateien...
+REM mkdir classes
+javac -d classes src/*.java
+
 cd classes/
-java Client Room User Message FileHandler
+REM Erstelle die Manifestdatei
+REM echo Main-Class: Client > ClientManifest.txt
+
+REM Erstelle die JAR-Datei im Verzeichnis der .class-Dateien und füge die Manifestdatei hinzu
+
+jar cfm Client.jar ClientManifest.txt *.class
+
+REM Überprüfe, ob die JAR-Datei erfolgreich erstellt wurde
+if exist Client.jar (
+    echo JAR-Datei Server.jar wurde erfolgreich erstellt.
+) else (
+    echo Fehler beim Erstellen der JAR-Datei!
+)
+
+REM Starte den Client
+echo Starte Client...
+java -jar Client.jar
+
 cd ../
+pause
